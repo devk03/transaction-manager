edc5c878196081076aa3e7d272e7094104430ecb
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-02-23 08:26:38 -0500

    CSV reading practice

---
+++ server/csv_parsing.py
@@ -0,0 +1,47 @@
+import csv  # import the csv module
+
+filename = "sample.csv"
+
+fields = []
+rows = []
+
+# reading as lists
+try:
+    with open(filename, "r") as csvfile:
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        for row in csvreader:
+            rows.append(row)
+except Exception as error:
+    print(error)
+
+print(fields)
+print(rows)
+
+
+with open(filename, mode="r") as file:
+    # Create a CSV reader with DictReader
+    csv_reader = csv.DictReader(file)
+
+    # Initialize an empty list to store the dictionaries
+    data_list = []
+
+    # Iterate through each row in the CSV file
+    for row in csv_reader:
+        # Append each row (as a dictionary) to the list
+        data_list.append(row)
+    print(data_list)
+
+""" Different types of sorts. """
+list_of_tuples = [(1, "f"), (2, "a"), (3, "c")]
+sorted_list = sorted(list_of_tuples, key=lambda x: x[1], reverse=True)
+print(sorted_list)  # Output: [(2, 'a'), (3, 'c'), (1, 'f')]
+
+# Sorting a list of integers in ascending order
+numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+numbers.sort()
+print(numbers)  # Output: [1, 1, 2, 3, 4, 5, 6, 9]
+
+# Sorting in descending order
+numbers.sort(reverse=True)
+print(numbers)  # Output: [9, 6, 5, 4, 3, 2, 1, 1]
